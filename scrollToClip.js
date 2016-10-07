438e0992bd5c5da266ab84c64d5e7580e257585b
Lucas Bersier <bers.lucas@gmail.com>
2016-10-07 09:20:39 -0400

    Update scrollToClip.js

--- scrollToClip.js
+++ scrollToClip.js
@@ -1,3 +1,51 @@
+<!doctype>
+<head>
+
+<meta name="viewport" content="minimal-ui, width=device-width, user-scalable=no"/>
+
+</head>
+
+<body>
+  <div class="logo">Test</div>
+  <div class="logo reverse">Test</div>
+  
+  <div id="red">
+  
+  </div>
+</body>
+
+<style>
+html,body{
+  margin: 0px;
+  background: #efefef;
+  height: 200%;
+}
+  #red{
+    width: 100%;
+    height: 200px;
+    background-color: #de5656;
+    position: relative;
+  }
+  
+  .logo{
+    position: fixed;
+    top: 10px;
+    left: 10px;
+    font-size: 50px;
+    color: #de5656;
+    z-index: 2;
+    clip: rect(50px, 300px, 60px, 0px)
+  }
+  
+  .reverse{
+    color: #fff;
+    z-index:1;
+    clip: inherit !important;
+  }
+</style>
+
+<script src="js/jquery.js"></script>
+<script>
 /*
 scrollToClip
 Sept. 6th, 2016
@@ -24,6 +72,10 @@ function scrollToClip(elem1,elem2,background){
   
   //Event listener. Toggled by wheel (scroll) movements
   document.addEventListener("wheel", function(){
+    scrollToClipFunc(elem1,elem2,background)
+  },false);
+  
+  document.addEventListener("touchmove", function() {
     scrollToClipFunc(elem1,elem2,background)
   },false);
 }  
@@ -57,3 +109,6 @@ function scrollToClipFunc(elem1, elem2, background){
     elem2.setAttribute("style","clip: rect(0px,100vw,0px, 0px) !important");
   } 
 }
+
+
+</script>
